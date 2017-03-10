88d4e93190b58222220d86d6b012409a84d3df9c
Arief Koesdwiady <ariefbarkah@gmail.com>
2017-03-10 11:38:38 -0500

    create mfcc extractor

---
+++ get-mfcc.py
@@ -0,0 +1,52 @@
+import echo_canc_lib as ec
+import os
+import numpy as np
+import glob
+
+path_txt = "/home/abkoesdw/Documents/cognitech/keyword-project/mfctxt-files-arief/"
+path_label = "/home/abkoesdw/Documents/cognitech/keyword-project/"
+path_mfcc = "/home/abkoesdw/Documents/cognitech/keyword-project/mfcc_new/"
+
+if not os.path.exists(path_mfcc):
+    os.makedirs(path_mfcc)
+
+files = glob.glob(path_mfcc + '*')
+for file_ in files:
+    os.remove(file_)
+
+with open(path_label + 'label_file') as f:
+    labels = f.readlines()
+
+with open(path_txt + 'trigger.mfc') as f:
+    data = f.readlines()
+data = np.asarray(data)
+flag_file = 0
+count = 0
+mfcc = np.empty((39, 0))
+filename = '______'
+i = 0
+file_id = 0
+while i < len(data):
+    x = data[i].split()
+    if x[0] == 'FILE:':
+        file_id += 1
+        prev_fn = filename
+        np.savetxt(path_mfcc + filename, mfcc.T)
+        filename_temp = x[1].split('/')[-1:]
+        filename_temp = np.asarray(filename_temp).astype(np.str)
+
+        filename = filename_temp[0][:-4] + '_' + str(file_id)
+
+        print(filename)
+
+        mfcc = np.empty((39, 0))
+    else:
+        x = np.asarray(x).astype(np.float)
+        mfcc = np.concatenate((mfcc, x.reshape(39, 1)), axis=1)
+
+    if file_id == 2379 and i == len(data) - 1:
+        print('at 2379', filename)
+        np.savetxt(path_mfcc + filename, mfcc.T)
+    i += 1
+
+print(file_id)
